fd075fcfc4f48a8dd630a682e793760d4c8bf5d0
devalexssantos <dev.alexssantos@gmail.com>
2023-08-20 15:57:51 -0300

    Subindo mais um teste

---
+++ java/01-fundamentos-java/02-sintaxe-java/01-anatomia-da-class/MinhaClasse/src/MinhaClasse3.java
@@ -0,0 +1,6 @@
+public class MinhaClasse3 {
+    public static void main (String [] args) {
+        System.out.println ("Olá, eu me chamdo Alex");
+        System.out.print ("Estou iniciando Java, mas já manjo de JS há algum tempo");
+    }
+}
