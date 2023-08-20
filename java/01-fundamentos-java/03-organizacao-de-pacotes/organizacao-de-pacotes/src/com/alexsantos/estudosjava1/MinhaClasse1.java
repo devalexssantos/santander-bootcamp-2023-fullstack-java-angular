91de8cb1af89bea4543022b646fa9e8d72cf71ea
devalexssantos <dev.alexssantos@gmail.com>
2023-08-20 17:25:52 -0300

    adicionando novo pacote

---
+++ java/01-fundamentos-java/03-organizacao-de-pacotes/organizacao-de-pacotes/src/com/alexsantos/estudosjava1/MinhaClasse1.java
@@ -0,0 +1,8 @@
+package com.alexsantos.estudosjava1;
+
+
+public class MinhaClasse1 {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Package 1");
+    }
+}
---
+++ java/01-fundamentos-java/03-organizacao-de-pacotes/organizacao-de-pacotes/src/com/alexsantos/estudosjava2/MinhaClasse2.java
@@ -0,0 +1,8 @@
+package com.alexsantos.estudosjava2; //Caminho do pacote chamado para buscar o arquivo
+
+
+public class MinhaClasse2 {    
+    public static void main (String [] args) {
+        System.out.print ("Package 2");
+    }
+}
