ebbe1f1bb27166785817b055b7185552991bb99e
devalexssantos <dev.alexssantos@gmail.com>
2023-09-04 22:33:27 -0300

    Subindo aula de argumento e terminal

---
+++ java/01-fundamentos-java/07-terminal-argumentos/MinhaClasse/src/AboutMe.java
@@ -0,0 +1,12 @@
+public class AboutMe {
+
+    public static void main(String [] arg) {
+        String nome = arg[0];
+        String sobreNome = arg[1];
+        String idade = arg[2];
+        String altura = arg[3];
+
+        System.out.println("Olá, meu nome é: " + nome + " " + sobreNome + ", tenho: " + idade + " anos e tenho: " + altura + "m de altura.");
+    }
+
+}
