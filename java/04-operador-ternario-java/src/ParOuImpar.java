3c236e8a6c269bebdf4ac9cb70530e1f7a066b5a
devalexssantos <dev.alexssantos@gmail.com>
2023-09-21 20:06:53 -0300

    Aula operador ternário Java

---
+++ java/04-operador-ternario-java/src/ParOuImpar.java
@@ -0,0 +1,9 @@
+public class ParOuImpar {
+    public static void main(String[] args) {
+
+        int numero = 6512;
+        String resultado = numero % 2 == 0 ? "O número: " + numero + " é PAR" : "O número: " + numero + " é IMPAR" ;
+
+        System.out.println(resultado);
+    }
+}
