f0d387789a051875c554cb937c5b132dbf64245e
devalexssantos <dev.alexssantos@gmail.com>
2023-09-27 22:36:34 -0300

    Aula for each em Java

---
+++ java/03-estruturas-de-repeticao-java/01-for-java/src/ForEach.java
@@ -0,0 +1,12 @@
+public class ForEach {
+
+    public static void main(String[] args) {
+        String apostolos [] = {"Pedro", "Tiago", "João", "André", "Simão", "Bartolomeu", "Felipe", "Tomé", "Mateus"}; 
+   
+       for(String apostolo : apostolos) {
+            System.out.println("Apostolo: " + apostolo);
+       }
+   
+    }
+
+}
